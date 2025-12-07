c8f64cdf871d83bba171eae53df2ec44f36f230f
Aryan Verma <aryanv2204@gmail.com>
2025-12-07 18:32:58 +0530

    success

--- client/src/components/auth/RegisterForm.jsx
+++ client/src/components/auth/RegisterForm.jsx
@@ -38,7 +38,7 @@ const submit = async (e) => {
         });
 
         console.log(responseData)
-        const { message } = responseData; 
+        const { message , success } = responseData; 
 
         if (success) { 
             toast.success(message || 'Registration successful!', { id: loadingToast });
